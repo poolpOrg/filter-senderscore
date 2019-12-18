poolpOrg/filter-senderscore

commit da6f00ab0d9fca524ad4076b4f9014ed8366df61

Synchronize outputs
Avoid printing to stdout from different Goroutines which may potentially
lead to mangled output. Instead, send all outputs to a single channel
and have a consumer receive these outputs and print them sequentially.

Fixes GitHub issue #10.

filter-senderscore.go
@@ -37,6 +37,8 @@ var scoreHeader *bool
 
 var version string
 
+var outputChannel chan string
+
 type session struct {
 	id string
 
@@ -130,6 +132,19 @@ func filterConnect(phase string, sessionId string, params[] string) {
 	}
 }
 
+func produceOutput(msgType string, sessionId string, token string, format string, a ...interface{}) {
+	var out string
+
+	if version < "0.5" {
+		out = msgType + "|" + token + "|" + sessionId
+	} else {
+		out = msgType + "|" + sessionId + "|" + token
+	}
+	out += fmt.Sprintf(format, a)
+
+	outputChannel <- out
+}
+
 func dataline(phase string, sessionId string, params[] string) {
 	token := params[0]
 	line := strings.Join(params[1:], "|")
@@ -137,20 +152,12 @@ func dataline(phase string, sessionId string, params[] string) {
 	s := sessions[sessionId]
 	if s.first_line == true {
 		if (s.score != -1 && *scoreHeader) {
-			if version < "0.5" {
-				fmt.Printf("filter-dataline|%s|%s|X-SenderScore: %d\n", token, sessionId, s.score)
-			} else {
-				fmt.Printf("filter-dataline|%s|%s|X-SenderScore: %d\n", sessionId, token, s.score)
-			}
+			produceOutput("filter-dataline", sessionId, token, "X-SenderScore: %d", s.score)
 		}
 		s.first_line = false
 	}
 	sessions[sessionId] = s
-	if version < "0.5" {
-		fmt.Printf("filter-dataline|%s|%s|%s\n", token, sessionId, line)
-	} else {
-		fmt.Printf("filter-dataline|%s|%s|%s\n", sessionId, token, line)
-	}
+	produceOutput("filter-dataline", sessionId, token, "%s", line)
 }
 
 func delayedAnswer(phase string, sessionId string, params[] string) {
@@ -185,24 +192,14 @@ func waitThenAction(sessionId string, token string, delay int, action string) {
 	if (delay != -1) {
 		time.Sleep(time.Duration(delay) * time.Millisecond)
 	}
-	if version < "0.5" {
-		fmt.Printf("filter-result|%s|%s|%s\n", token, sessionId, action)
-	} else {
-		fmt.Printf("filter-result|%s|%s|%s\n", sessionId, token, action)
-	}
-	return
+	produceOutput("filter-result", sessionId, token, "%s", action)
 }
 
 func waitThenDisconnect(sessionId string, token string, delay int) {
 	if (delay != -1) {
 		time.Sleep(time.Duration(delay) * time.Millisecond)
 	}
-	if version < "0.5" {
-		fmt.Printf("filter-result|%s|%s|disconnect|550 your IP reputation is too low for this MX\n", token, sessionId)
-	} else {
-		fmt.Printf("filter-result|%s|%s|disconnect|550 your IP reputation is too low for this MX\n", sessionId, token)
-	}
-	return
+	produceOutput("filter-result", sessionId, token, "disconnect|550 your IP reputation is too low for this MX")
 }
 
 func filterInit() {
@@ -257,6 +254,13 @@ func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	skipConfig(scanner)
 	filterInit()
+
+	outputChannel = make(chan string)
+	go func() {
+		for line := range outputChannel {
+			fmt.Println(line)
+		}
+	}()
 
 	for {
 		if !scanner.Scan() {

commit 50aa535035dd448ef8dbfe974bb881af6f2eb773

Add missing separator to outputs
Also, use the correct syntax to pass additional parameters to
fmt.Sprintf().

Fixes a regression introduced in commit da6f00a (Synchronize outputs,
2019-12-17).

filter-senderscore.go
@@ -143,7 +143,7 @@ func produceOutput(msgType string, sessionId string, token string, format string
 	} else {
 		out = msgType + "|" + sessionId + "|" + token
 	}
-	out += fmt.Sprintf(format, a)
+	out += "|" + fmt.Sprintf(format, a...)
 
 	outputChannel <- out
 }