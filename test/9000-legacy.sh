d250d1802a542e783b91fd1e75b13ed0f2917366
Lukas Fleischer <lfleischer@lfos.de>
2020-01-02 16:09:32 +0100

    test/9000-legacy.sh: test with high protocol version
    Add a test case to verify that high protocol versions such as 0.42 do
    not trigger legacy mode.

--- test/9000-legacy.sh
+++ test/9000-legacy.sh
@@ -16,4 +16,16 @@ test_run 'test with protocol version 0.4' '
 	test_cmp actual expected
 '
 
+test_run 'test with protocol version 0.42' '
+	cat <<-EOD | "$FILTER_BIN" $FILTER_OPTS -blockBelow 20 | sed "0,/^register|ready/d" >actual &&
+	config|ready
+	report|0.42|0|smtp-in|link-connect|7641df9771b4ed00||pass|1.2.3.100:33174|1.1.1.1:25
+	filter|0.42|0|smtp-in|connect|7641df9771b4ed00|1ef1c203cc576e5d||pass|1.2.3.100:33174|1.1.1.1:25
+	EOD
+	cat <<-EOD >expected &&
+	filter-result|7641df9771b4ed00|1ef1c203cc576e5d|proceed
+	EOD
+	test_cmp actual expected
+'
+
 test_complete
